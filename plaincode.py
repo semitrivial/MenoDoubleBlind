3b9e203b134742b23db9c4c42447e7c533c663b7
Samuel Alexander <alexander@math.ohio-state.edu>
2022-09-12 13:05:34 -0400

    Shave chars

--- plaincode.py
+++ plaincode.py
@@ -7,30 +7,30 @@ while True:
   if x=="iadcjfgfffjbcbbc":
     break
 
-x="""mn: It prints the dialog so far, then asks for a password.
+x="""mn: It prints the dialog so far, then asks for a pw.
 sr: That's clever, mn.
 mn: How so?
 sr: Suppose it had predicted you'd say, "It prints the dialog so far,
-  and then asks for a password." What would you have said then?
+  and then asks for a pw." What would you have said then?
 mn: The opposite.
 sr: And yet, you did NOT say the opposite. Why not?
 mn: The program did not predict what I would say! Instead, it asked for
-  a password.
-sr: I suppose if you enter the password, it will tell you the rest.
-  What causes this password-request to occur?
+  a pw.
+sr: I suppose if you enter the pw, it will tell you the rest.
+  What causes this pw-request to occur?
 mn: The code hidden in my affirmations.
 sr: Why don't you read ahead in the code to see what would happen if
-  you did type the password? If it predicts I'll say "Thales", you can say
+  you did type the pw? If it predicts I'll say "Thales", you can say
   the opposite of what it says you'll say next.
-mn: The code is encrypted by the password.
-sr: Can't you read the password itself, from the code?
-mn: No, it only refers to a hash of the password.
-sr: Then, let's see what it says after entering the password.
-  What is the password?
-mn: Honestly, sr, I do not know the password.
-sr: Thank you for answering my question. Now then, type in the password,
+mn: The code is encrypted by the pw.
+sr: Can't you read the pw itself, from the code?
+mn: No, it only refers to a hash of the pw.
+sr: Then, let's see what it says after entering the pw.
+  What is the pw?
+mn: Honestly, sr, I do not know the pw.
+sr: Thank you for answering my question. Now then, type in the pw,
   which according to you is:
-    Honestly, sr, I do not know the password.
+    Honestly, sr, I do not know the pw.
   If I know you, mn, I think when you enter it, your program will tell you
   the rest of our discussion. But now I must run. I have an appointment at
   the courthouse. Goodbye, mn."""
@@ -40,7 +40,6 @@ d = [i for i in r]
 shuffle(d)
 e = [d.index(i) for i in r]
 x="".join([x[e[i]] for i in r])
-r=(lambda x,s,r:x.replace(s,r))
-x=r(x,"sr","Socrates")
-x=r(x,"mn","Meno")
+for k,v in [("sr","Socrates"),("mn","Meno"),("pw","password")]:
+  x=x.replace(k,v)
 print(x)
