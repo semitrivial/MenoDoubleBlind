21269d8fc1d6440e8d879e56107d33412423fe5d
Samuel Alexander <alexander@math.ohio-state.edu>
2022-09-12 12:32:38 -0400

    +gen_code.py

---
+++ gen_code.py
@@ -0,0 +1,28 @@
+fp = open("plaincode.py", "r")
+code = fp.read()
+fp.close()
+start = code.index('"""')
+before = code[:start]
+secret = code[start+3:]
+end = secret.index('"""')
+after = secret[end+3:]
+secret = secret[:end]
+
+pwd = "Honestly, Socrates, I do not know the password."
+from random import seed, shuffle
+r=range(len(secret))
+seed(hash(pwd+"_"))
+d = [i for i in r]
+shuffle(d)
+secret = "".join([secret[d[i]] for i in r])
+
+coded = before + '"""' + secret + '"""' + after
+fp = open("coded.py", "w")
+fp.write(coded)
+fp.close()
+
+fp = open("skeleton.txt", "r")
+dialog = fp.read()
+fp.close()
+lines = dialog.splitlines()
+print(f'Need: {len(coded)/2}, Have: {lines.count("Meno: YES.")}')
--- plaincode.py
+++ plaincode.py
@@ -1,51 +1,42 @@
-a = alphabet
+a,h=alphabet,hash
 while True:
-  pwd = input("Password: ")
-  x = "".join([alphabet[int(x)] for x in str(abs(hash(pwd)))])
-  if x == "gbgjcagdhdhecgiiiij":
+  p=input("Password:")
+  x="".join([a[int(x)] for x in str(abs(h(p)))])
+  if x=="gbgjcagdhdhecgiiiij":
     break
 
-secret="""Meno: It prints the dialog so far, and then asks for a password.
-Socrates: That is clever, Meno.
-Meno: What do you mean?
-Socrates: Suppose it had printed what it did, followed by:
-    Meno: It prints the dialog so far, and then asks for a password.
-  What would you have said then?
-Meno: I would have said the exact opposite of,
-    It prints the dialog so far, and then asks for a password.
-Socrates: And yet, you did NOT say the opposite of
-    It prints the dialog so far, and then asks for a password.
-  Why not?
-Meno: Because the program did not say I would say that! Instead, it asked
-  for a password!
-Socrates: I suppose once you enter the password, then it will tell you
-  the rest. And this behavior occurs when the code hidden in your
-  affirmations is executed?
-Meno: Yes.
-Socrates: Why didn't you simply read the code to find out what it would
-  print if you enter the password? For example, if it said,
-    if pwd == "Homer":
-      print("Meno: It prints the dialog so far, and then asks for a password.")
-  then you would have known to say the opposite. So why didn't you just
-  read the code to see what happens after the password is entered?
-Meno: The code is encrypted, and uses the password to decrypt itself.
-Socrates: Can't you read the password itself, from the code? For instance,
-  if the code says, "if pwd == 'Homer':", then the password is Homer?
+x="""Meno: It prints the dialog so far, then asks for a password.
+Socrates: That's clever, Meno.
+Meno: How so?
+Socrates: Suppose it had predicted you'd say, "It prints the dialog so far,
+  and then asks for a password." What would you have said then?
+Meno: The opposite.
+Socrates: And yet, you did NOT say the opposite. Why not?
+Meno: The program did not predict what I would say! Instead, it asked for
+  a password.
+Socrates: I suppose if you enter the password, it will tell you the rest.
+  What causes this password-request to occur?
+Meno: The code hidden in my affirmations.
+Socrates: Why don't you read ahead in the code to see what would happen if
+  you did type the password? If it predicts I'll say "Thales", you can say
+  the opposite of what it says you'll say next.
+Meno: The code is encrypted by the password.
+Socrates: Can't you read the password itself, from the code?
 Meno: No, it only refers to the hash of the password.
-Socrates: Well then, let's see what it says after entering the password.
+Socrates: Then, let's see what it says after entering the password.
   What is the password?
 Meno: Honestly, Socrates, I do not know the password.
 Socrates: Thank you, Meno, for answering my question. Now then, type in the
   password, which according to you is:
     Honestly, Socrates, I do not know the password.
-  And if I know you, Meno, I think when you enter it, your program will
-  tell you the rest of our discussion. But now I must run, for I have an
-  appointment at the courthouse. Farewell, dear Meno."""
+  If I know you, Meno, I think when you enter it, your program will tell you
+  the rest of our discussion. But now I must run. I have an appointment at
+  the courthouse. Goodbye, Meno."""
 
 from random import seed, shuffle
-r=range(len(secret))
-seed(hash(pwd+"_"))
+r=range(len(x))
+seed(h(pwd+"_"))
 d = [i for i in r]
-d.shuffle()
+shuffle(d)
 e = [d.index(i) for i in r]
-print("".join([secret[e[i]] for i in r]))
+print("".join([x[e[i]] for i in r]))
