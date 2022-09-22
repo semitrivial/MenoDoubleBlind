28c34999e0a0b16048be106fcd78cbbcd37c11b8
Samuel Alexander <alexander@math.ohio-state.edu>
2022-09-22 14:48:26 -0400

    More shaving

--- plaincode.py
+++ plaincode.py
@@ -8,16 +8,15 @@ while True:
 x="""s,r,p='|it asks for a pw.','predict','|honestly, sr, |i do not know the pw.'
 x=f'''mn: {s}
 sr: |it didn't {r} you'd say: {s}?
-mn: |no, or I would've said the opposite.
-sr: |maybe once you enter the pw, it will show its {r}ion.
-  |why don't you read ahead in the code, analyze and anticipate it?
+mn: |no, or I'd have said the opposite.
+sr: |maybe the {r}ion appears if you enter the pw. |why don't
+  you read ahead in the code, analyze and anticipate it?
 mn: |the code is encrypted.
 sr: |then let us enter the password. |what is the pw?
 mn: {p}
-sr: |go ahead, then. |enter the pw, which according to you is:
+sr: |good. |enter the pw, which according to you is:
     {p}
-  |maybe then it will {r} the rest of our discussion. |but now |i must
-  depart, mn. Goodbye.'''
+  |maybe then it will {r} your line, '{s}'.'''
 for k,v in[("sr","Socrates"),("mn","Meno"),("pw","password")]:
   x=x.replace(k,v)
 print(x)"""
