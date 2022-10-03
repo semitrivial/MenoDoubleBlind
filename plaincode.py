61d58a32ac1ed752b0282e5923e82bb5c0cc993d
Samuel Alexander <alexander@math.ohio-state.edu>
2022-10-03 16:24:30 -0400

    Meno's program only needs to predict MENO'S lines

--- plaincode.py
+++ plaincode.py
@@ -4,17 +4,10 @@ while 1:
  a.seed (p)
  if "". join(a.sample(p,10))=="a.'nodorss":
   break
-x="""print(f'''Meno: ocrat.
-Socrates: It didn't predict your 'ocrat'?
+x="""print(f'''Meno: It asks for a passphrase.
 Meno: No, or I'd have said the opposite.
-Socrates: Maybe the prediction appears if you input the passphrase. And you 
-  can study the code first and predict your answer?
 Meno: It's encrypted.
-Socrates: Then input the passphrase! What is it?
-Meno: crate
-Socrates: Good, now input the passphrase, which you say is:
-    crate
-  Maybe then it will predict your 'ocrat'.'''.replace('ocrat','It asks for a passphrase'))"""
+Meno: '''+p)"""
 d=[i for i in range(len(x))]
 a.shuffle(d)
-exec("".join(x[i]for i in d).replace('crate',p))
+exec("".join(x[i]for i in d)
