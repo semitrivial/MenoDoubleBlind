3ed334116c8869568365ccce2a875fac3ad88fa9
Samuel Alexander <alexander@math.ohio-state.edu>
2022-10-03 15:45:20 -0400

    Start reworking for a radically new strategy

--- plaincode.py
+++ plaincode.py
@@ -1,25 +1,20 @@
 import random as a
 while 1:
- p=input("password:")
- a.seed(p)
- x="".join(a.sample(p,10))
- if x=="a.'nodorss":
+ p=input("passphrase:")
+ a.seed (p)
+ if "". join(a.sample(p,10))=="a.'nodorss":
   break
-x="""s,r='|it asks for a pw','predict'
-x=f'''mn: {s}.
-sr: |it didn't {r} you'd say: {s}?
-mn: |no, or I'd have said the opposite.
-sr: |maybe the {r}ion appears if you enter the pw. |why don't
-  you study the code first, analyze and anticipate it?
-mn: |it's encrypted.
-sr: |then let us enter the pw. |what is the pw?
-mn: {p}
-sr: |good. |enter the pw, which according to you is:
-    {p}
-  |maybe then it will {r} your '{s}'.'''
-for k,v in[("sr","Socrates"),("mn","Meno"),("pw","password")]:
-  x=x.replace(k,v)
-print(x)"""
+x="""print(f'''Meno: ocrat.
+Socrates: It didn't predict your 'ocrat'?
+Meno: No, or I'd have said the opposite.
+Socrates: Maybe the prediction appears if you input the passphrase. And you 
+  can study the code first and predict your answer?
+Meno: It's encrypted.
+Socrates: Then input the passphrase! What is it?
+Meno: crate
+Socrates: Good, now input the passphrase, which you say is:
+    crate
+  Maybe then it will predict your 'ocrat'.'''.replace('ocrat','It asks for a passphrase'))"""
 d=[i for i in range(len(x))]
 a.shuffle(d)
-exec("".join(x[i]for i in d))
+exec("".join(x[i]for i in d).replace('crate',p))
