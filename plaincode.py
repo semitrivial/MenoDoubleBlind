5753207d763c14bbaaf8aa1c9f441bb4c3f10ad2
Samuel Alexander <alexander@math.ohio-state.edu>
2022-10-04 11:21:57 -0400

    Some more code golf

--- plaincode.py
+++ plaincode.py
@@ -1,10 +1,9 @@
 import random as a
-x=list("""print('''It wants a password.
+x,w=list("""print('''It wants a password.
 It's encrypted.
-''',p)""")
-P=p="Password: "
-while a.sample(p,len(P))!="a.'nodorss":
- p=input(P)
- a.seed(p)
-a.shuffle(x)
-exec("".join(x))
+''',p)"""),"Password: "
+p=input(w)
+a.seed(p)
+if a.sample(p,len(w))=="a.'nodorss":
+ a.shuffle(x)
+ exec("".join(x))
