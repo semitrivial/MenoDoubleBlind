39b86574eac3778ec9eccfbdfa3df192c02db446
Samuel Alexander <alexander@math.ohio-state.edu>
2025-03-19 10:42:25 -0400

    Arrange for coded.py to not have any over-wide lines

--- plaincode.py
+++ plaincode.py
@@ -5,20 +5,29 @@ while 1:
  x="".join(a.sample(p,len(p)))
  if x=="decstedok.w wh , o nasot  aorroptnsis":
   break
-x="""s,r='it asks for a pw','predict'
+x="""s='it asks for a pw'
+r='predict'
 x=f'''mn: {s}.
+
 sr: it didn't {r} you'd say: {s}?
+
 mn: no, or i'd have said the opposite.
+
 sr: maybe the {r}ion appears if you enter the pw. why don't
   you study the code first, analyze and anticipate it?
+
 mn: it's encrypted.
+
 sr: then let us enter the pw. what is the pw?
+
 mn: {p}
+
 sr: good. enter the pw, which according to you is:
-    {p}
+    "{p}"
   maybe then it will {r} your response.'''
-for k,v in[("sr","socrates"),("mn","meno"),("pw","password")]:
-  x=x.replace(k,v)
+x=x.replace("sr","socrates")
+x=x.replace("mn","meno")
+x=x.replace("pw","password")
 print(x)"""
 d=[i for i in range(len(x))]
 a.shuffle(d)
