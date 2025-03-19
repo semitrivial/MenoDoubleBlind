e97f1756f9605f6b526acf45592b6907d2d85aee
Samuel Alexander <alexander@math.ohio-state.edu>
2025-03-19 13:19:06 -0400

    Code golf

--- plaincode.py
+++ plaincode.py
@@ -2,11 +2,9 @@ import random as a
 z,x,j=", yo ooned","""print(f'''it wants a password.
 it's encrypted.
 {p}''')""","".join
-while a:
- p=input("password: ")
- a.seed(p)
- if j(a.sample(p,len(z)))==z:
-  break
-d=[i for i in range(len(x))]
-a.shuffle(d)
-exec(j(x[i]for i in d))
+p=input("password: ")
+a.seed(p)
+if j(a.sample(p,len(z)))==z:
+ d=[i for i in range(len(x))]
+ a.shuffle(d)
+ exec(j(x[i]for i in d))
