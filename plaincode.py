d1db05c2479997b436f5c19f20a80263c9b28453
Samuel Alexander <alexander@math.ohio-state.edu>
2025-03-19 12:39:27 -0400

    Minor code golf

--- plaincode.py
+++ plaincode.py
@@ -1,8 +1,9 @@
 import random as a
+j="".join
 while a:
  p=input("Password: ").lower()
  a.seed(p)
- x="".join(a.sample(p,len(p)))
+ x=j(a.sample(p,len(p)))
  if x=="ttonaws on'kwyi adnpe rs ocos,dar .s":
   break
 x="""print(f'''it wants a password.
@@ -10,4 +11,4 @@ it's encrypted.
 {p}''')"""
 d=[i for i in range(len(x))]
 a.shuffle(d)
-exec("".join(x[i]for i in d))
+exec(j(x[i]for i in d))
