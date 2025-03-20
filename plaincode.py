eac7b71b8737693431ecd73b4063c93babd6d243
Samuel Alexander <alexander@math.ohio-state.edu>
2025-03-20 12:54:01 -0400

    Slight tweak

--- plaincode.py
+++ plaincode.py
@@ -1,8 +1,8 @@
 import random as a
+p=input("password: ")
 z,x,j=", yo ooned","""print(f'''it wants a password.
 it's encrypted.
 {p}''')""","".join
-p=input("password: ")
 a.seed(p)
 if j(a.sample(p,len(z)))==z:
  d=list(range(len(x)))
