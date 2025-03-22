b99d2d81f775b7d58cb966177cd711a08abfe5fd
Samuel Alexander <alexander@math.ohio-state.edu>
2025-03-22 17:01:17 -0400

    Major revelation: Switch to a different approach

--- plaincode.py
+++ plaincode.py
@@ -1,10 +1,8 @@
-#trojan horse
-import random as r
-p=input("password: ")
+#trojn hrse
+seed(p=askpword())
 z,x=", yo ooned","""print('''it wants a password.
 it's encrypted.
 '''+p)"""
-d,j=list(range(len(x))),"".join
-s,c=r.seed(p),j(r.sample(p,len(z)))
-r.shuffle(d)
-if c==z:exec(j(x[i]for i in d))
+c=jn(smpl(p,z.len
+shfl(d=rng(x.len
+if c==z:exec(jn(x.i:i in d
