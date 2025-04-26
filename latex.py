dad8205867c1985b68c1974d75592d67904d96d4
Samuel Alexander <alexander@math.ohio-state.edu>
2025-04-26 13:07:42 -0400

    +latex.py

--- latex.py
+++ latex.py
@@ -25,7 +25,30 @@ intro = introfp.read()
 introfp.close()
 latex += intro
 
-latex += "(Excerpt goes here)"
+dialogfp = open("excerpts.txt", "r")
+dialog = dialogfp.read()
+dialog = dialog.replace("\\", "$\\backslash$")
+dialogfp.close()
+lines = dialog.splitlines()
+
+
+nested_lines = []
+for line in lines:
+  if line=='':
+    continue
+
+  if not(line.startswith(" ")):
+    if nested_lines:
+      latex += r"\begin{quote}"
+      for nested in nested_lines:
+        latex += f"{nested}\\\\\n"
+      nested_lines = []
+      latex += r"\end{quote}"
+
+    latex += "\n\\noindent " + line + "\n"
+  else:
+    nested_lines.append(line)
+
 
 outrofp = open("outro.txt", "r")
 outro = outrofp.read()
@@ -39,6 +62,8 @@ latex += r"""\end{document}
 
 latex = latex.replace('"', r'{\textquotedbl}')
 latex = latex.replace("'", r'{\textquotesingle}')
+latex = latex.replace("_", r"\_")
+latex = latex.replace('#', r"\#")
 
 outfp=open("meno2.tex", "w")
 outfp.write(latex)
